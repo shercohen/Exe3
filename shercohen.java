65192da1d9c4fcac5ca996a9ec88ea8aac83418b
sher <shercohen3@gmail.com>
2023-03-13 15:03:23 +0200

    The file was change

--- shercohen.java
+++ shercohen.java
@@ -1,7 +1,7 @@
 public class shercohen{
     public static void main(String[] args){
         for(int i=1; i<=10; i++){
-            System.out.println("SherCohen...");
+            System.out.println("SherSaraCohen...");
         }
 
 
